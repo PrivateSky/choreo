e9e2b9b58a0584c8877286da3d5186f5acba5f0f
salboaie <salboaie@gmail.com>
2019-11-18 13:14:13 +0200

    multiple blockchains could be loaded simultaneously

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 let cm = require("callflow");
-
-
 let swarmUtils = require("./swarmTypes/swarm_template");
 
 
@@ -8,50 +6,3 @@ $$.swarms           = cm.createSwarmEngine("swarm", swarmUtils);
 $$.swarm            = $$.swarms;
 
 $$.swarmInstanceManager = require("./swarmTypes/choreographies/swarmInstancesManager");
-
-function SwarmEngine(swarmCommunicationStrategy, nameService, serialisationStrategy, securityContext){
-
-    this.init = function(identity){
-
-    }
-
-    this.stop = function(){
-
-    }
-
-}
-
-let psc = require("../../psk-security-context");
-
-module.exports = {
-    initialiseSwarmEngine:function(swarmCommunicationStrategy, nameService, serialisationStrategy, securityContext){
-        return new SwarmEngine(swarmCommunicationStrategy, nameService, serialisationStrategy, securityContext);
-    },
-    createSerialisationStrategy:function(strategyName, ...args){
-        switch(strategyName){
-            case "json":
-            case "JSON":return require("strategies/JSONSerialisationStrategy").createStrategy(args);
-            default: console.error("Unknown strategy ", strategyName);
-        }
-        return undefined;
-    },
-    createCommunicationStrategy:function(strategyName, ...args){
-        switch(strategyName){
-            case "fake":return require("strategies/fakeCommunicationStrategy").createStrategy(args);
-            case "sandbox":return require("strategies/fakeCommunicationStrategy").createStrategy(args);
-            case "serviceWorkers":return require("strategies/fakeCommunicationStrategy").createStrategy(args);
-            default: console.error("Unknown strategy ", strategyName);
-        }
-        return undefined;
-    },
-    createNameService:function(strategyName, ...args){
-        switch(strategyName){
-            case "default":return require("strategies/defaultNameService").createStrategy(args);
-            default: console.error("Unknown strategy ", strategyName);
-        }
-        return undefined;
-    },
-    createSecurityContext:function(strategyName, ...args){
-        return psc.createSecurityContext(strategyName,...args);
-    }
-}
