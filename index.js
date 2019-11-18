3a29ff66382de63f9ce40036a847fc77181d460f
salboaie <salboaie@gmail.com>
2019-11-18 13:26:24 +0200

    wip

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let cm = require("callflow");
 let swarmUtils = require("./swarmTypes/swarm_template");
 
+$$.swarmInstanceManager = require("./swarmTypes/choreographies/swarmInstancesManager");
+
 
 $$.swarms           = cm.createSwarmEngine("swarm", swarmUtils);
 $$.swarm            = $$.swarms;
-
-$$.swarmInstanceManager = require("./swarmTypes/choreographies/swarmInstancesManager");
