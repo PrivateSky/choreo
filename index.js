f3d4dea846f431be3c791cbcc3ca18feca5ef6fc
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-11-25 16:26:13 +0200

    Added initialized SwarmEngine

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let cm = require("callflow");
 let swarmUtils = require("./swarmTypes/swarm_template");
+const SwarmEngine = require('../swarm-engine');
 
 //$$.swarmInstanceManager = require("./swarmTypes/choreographies/swarmInstancesManager");
 
+let nameService = SwarmEngine.createNameService("default");
+let serialisationStrategy = SwarmEngine.createSerialisationStrategy("json");
+let swarmCommunicationStrategy = SwarmEngine.createCommunicationStrategy("local", nameService, serialisationStrategy);
+let securityContext = SwarmEngine.createSecurityContext();
+
+SwarmEngine.initialiseSwarmEngine(swarmCommunicationStrategy, nameService, serialisationStrategy, securityContext);
 
 $$.swarms           = cm.createSwarmEngine("swarm", swarmUtils);
 $$.swarm            = $$.swarms;
